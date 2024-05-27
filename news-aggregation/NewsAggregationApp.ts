86178c26f902f14f521c4da46fdafadb993098e3
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-05-28 00:54:14 +0530

    show direct message for user onInstall

--- news-aggregation/NewsAggregationApp.ts
+++ news-aggregation/NewsAggregationApp.ts
@@ -1,12 +1,29 @@
 import {
     IAppAccessors,
+    IAppInstallationContext,
+    IHttp,
     ILogger,
+    IModify,
+    IPersistence,
+    IRead,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { sendDirectMessageOnInstall } from './utils/message';
 
 export class NewsAggregationApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
+    }
+
+    public async onInstall(
+        context: IAppInstallationContext,
+        read: IRead,
+        http: IHttp,
+        persistence: IPersistence,
+        modify: IModify
+    ): Promise<void> {
+        const user = context.user;
+        await sendDirectMessageOnInstall(read, modify, user, persistence);
     }
 }
---
+++ news-aggregation/enums/messageEnum.ts
@@ -0,0 +1,13 @@
+export enum OnInstallContent {
+    PREVIEW_TITLE="[**News Aggregation App**](https://github.com/RocketChat/News-Aggregation-App)",
+    PREVIEW_DESCRIPTION = "**Installed and on your Server!**",
+    PREVIEW_CONTEXT = "[**Support's Page**](https://github.com/RocketChat/News-Aggregation-App/issues)",
+    PREVIEW_IMAGE = "",
+    WELCOMING_MESSAGE = `
+        Thank you for installing the \`News Aggregation App\`
+
+        Setting up the News Aggregation App is easy! This is temporary welcome message and it will be modified in the next commits to contain info about the app.
+
+    `,
+    WELCOME_TEXT = `Welcome to **News Aggregation App** in RocketChat!`,
+}
