bfeb95ccb2ad0fbd85217abe955c2fa20ec9d2e2
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-01 16:02:57 +0530

    TechCrunchNewsSource class setup + on demand slash command setup

--- news-aggregation/utils/commandUtility.ts
+++ news-aggregation/utils/commandUtility.ts
@@ -1,10 +1,21 @@
 import { IUser } from "@rocket.chat/apps-engine/definition/users";
-import { ICommandUtility, ICommandUtilityParams } from "../definitions/ICommandUtility";
+import {
+    ICommandUtility,
+    ICommandUtilityParams,
+} from "../definitions/ICommandUtility";
 import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
 import { SlashCommandContext } from "@rocket.chat/apps-engine/definition/slashcommands";
-import { IHttp, IModify, IPersistence, IRead } from "@rocket.chat/apps-engine/definition/accessors";
+import {
+    IHttp,
+    IModify,
+    IPersistence,
+    IRead,
+} from "@rocket.chat/apps-engine/definition/accessors";
 import { NewsAggregationApp } from "../NewsAggregationApp";
-
+import { CommandEnum } from "../enums/commandEnum";
+import { sendHelperMessage } from "./message";
+import { TechCrunchNewsSource } from "../news-sources/TechCrunchNewsSource";
+import { NewsItem } from "../definitions/NewsItem";
 export class CommandUtility implements ICommandUtility {
     sender: IUser;
     room: IRoom;
@@ -17,7 +28,7 @@ export class CommandUtility implements ICommandUtility {
     app: NewsAggregationApp;
 
     constructor(props: ICommandUtilityParams) {
-        this.app = props.app;
+        this.sender = props.sender;
         this.room = props.room;
         this.command = props.command;
         this.context = props.context;
@@ -28,7 +39,38 @@ export class CommandUtility implements ICommandUtility {
         this.app = props.app;
     }
 
-    public async resolveCommand(): Promise<void> {
+    private async helperMessage() {
+        await sendHelperMessage(
+            this.room,
+            this.read,
+            this.modify,
+            this.sender,
+            this.http,
+            this.persistence
+        );
+    }
 
+    private async fetchNewsFromSource() {
+        const news: NewsItem[] = [];
+        const techCrunchSource = new TechCrunchNewsSource(this.app, news);
+        await techCrunchSource.fetchNews(this.read, this.modify, this.room, this.http, this.persistence);
+    }
+
+    // public async getNewsFromSource() {
+    //     const news: NewsItem[] = [];
+    //     const techCrunchSource = new TechCrunchNewsSource(this.app, news);
+    //     await techCrunchSource.
+    // }
+
+    public async resolveCommand(): Promise<void> {
+        switch (this.command[0]) {
+            case CommandEnum.ALERT:
+                await this.fetchNewsFromSource();
+
+            case CommandEnum.HELP:
+            default:
+                await this.helperMessage();
+                break;
+        }
     }
 }
