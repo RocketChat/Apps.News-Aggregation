114a859f26ae7f13c322449d001f0432438c36ed
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-02 18:30:58 +0530

    command setup based on length of command

--- news-aggregation/enums/commandEnum.ts
+++ news-aggregation/enums/commandEnum.ts
@@ -1,3 +1,6 @@
 export enum CommandEnum {
+    ALERT = "alert",
+    SUBSCRIBE = "subscribe",
+    UNSUBSCRIBE = "unsubscribe",
     HELP = "help",
 }
--- news-aggregation/utils/commandUtility.ts
+++ news-aggregation/utils/commandUtility.ts
@@ -1,8 +1,16 @@
 import { IUser } from "@rocket.chat/apps-engine/definition/users";
-import { ICommandUtility, ICommandUtilityParams } from "../definitions/ICommandUtility";
+import {
+    ICommandUtility,
+    ICommandUtilityParams,
+} from "../definitions/ICommandUtility";
 import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
 import { SlashCommandContext } from "@rocket.chat/apps-engine/definition/slashcommands";
-import { IHttp, IModify, IPersistence, IRead } from "@rocket.chat/apps-engine/definition/accessors";
+import {
+    IHttp,
+    IModify,
+    IPersistence,
+    IRead,
+} from "@rocket.chat/apps-engine/definition/accessors";
 import { NewsAggregationApp } from "../NewsAggregationApp";
 import { CommandEnum } from "../enums/commandEnum";
 import { sendHelperMessage } from "./message";
@@ -41,9 +49,75 @@ export class CommandUtility implements ICommandUtility {
         );
     }
 
-    public async resolveCommand(): Promise<void> {
-        switch (this.command[0]) {
+    // private async fetchNewsFromSource() {
+    //     const news: NewsItem[] = [];
+
+    //     const techCrunchSource = new TechCrunchNewsSource(this.app, news);
+
+    //     await techCrunchSource.fetchNews(
+    //         this.read,
+    //         this.modify,
+    //         this.room,
+    //         this.http,
+    //         this.persistence
+    //     );
+    // }
+
+    // public async getNewsFromSource() {
+    //     const news: NewsItem[] = [];
+    //     const techCrunchSource = new TechCrunchNewsSource(this.app, news);
+    //     await techCrunchSource.
+    // }
+
+    public async subscribeNews() {
+        console.log("news subscribe working.");
+        this.app.getLogger().info("news subscribe working.");
+    }
+
+    public async unsubscribeNews() {
+        console.log("news unsubscribe working.");
+        this.app.getLogger().info("news unsubscribe working.");
+    }
+
+    private async handleSingleParamCommand() {
+        const singleParamCommand = this.command[0];
+
+        switch (singleParamCommand) {
+            case CommandEnum.ALERT:
+            // await this.fetchNewsFromSource();
+
+            case CommandEnum.SUBSCRIBE:
+                await this.subscribeNews();
+
+            case CommandEnum.UNSUBSCRIBE:
+                await this.unsubscribeNews();
+
             case CommandEnum.HELP:
+
+            default:
+                await this.helperMessage();
+                break;
+        }
+    }
+
+    private async handleDualParamCommand() {
+        console.log("dual param executed");
+        this.app.getLogger().info("dual param executed");
+    }
+
+    public async resolveCommand(): Promise<void> {
+        switch (this.command.length) {
+            case 1: {
+                await this.handleSingleParamCommand();
+                break;
+            }
+
+            case 2: {
+                await this.handleDualParamCommand();
+                break;
+            }
+
+            case 0:
             default:
                 await this.helperMessage();
                 break;
