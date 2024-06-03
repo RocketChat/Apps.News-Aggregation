8a7a512de2fc93a660af89d34a370569fb76afb5
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-03 11:58:23 +0530

    modify the base class and TechCrunch according to adapters

--- news-aggregation/definitions/NewsSource.ts
+++ news-aggregation/definitions/NewsSource.ts
@@ -9,15 +9,38 @@ import { NewsAggregationApp } from "../NewsAggregationApp";
 
 import { NewsItem } from "./NewsItem";
 import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
+import { INewsSourceAdapter } from "../adapters/INewsSourceAdapter";
 
 export class NewsSource {
+    private adapter: INewsSourceAdapter;
+    app: NewsAggregationApp;
     news: NewsItem[] = [];
 
-    constructor(app: NewsAggregationApp, news: NewsItem[]) {
+    constructor(
+        app: NewsAggregationApp,
+        adapter: INewsSourceAdapter,
+        news: NewsItem[],
+    ) {
+        this.app = app;
+        this.adapter = adapter;
         this.news = news;
     }
 
-    async fetchNews(read: IRead, modify: IModify, room: IRoom, http: IHttp, persis: IPersistence): Promise<any> {}
+    async fetchNews(
+        read: IRead,
+        modify: IModify,
+        room: IRoom,
+        http: IHttp,
+        persis: IPersistence,
+    ): Promise<any> {
+        return this.adapter.fetchNews(read, modify, room, http, persis);
+    }
 
-    async getNews(read: IRead, modify: IModify, room: IRoom, http: IHttp, persis: IPersistence): Promise<any> {}
+    async getNews(
+        read: IRead,
+        modify: IModify,
+        room: IRoom,
+        http: IHttp,
+        persis: IPersistence,
+    ): Promise<any> {}
 }
