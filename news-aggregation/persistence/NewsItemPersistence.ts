d4107473bb68041e9b36f0bc569dbf65fcec650f
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-04 19:21:56 +0530

    saveNews() persistence method

--- news-aggregation/persistence/NewsItemPersistence.ts
+++ news-aggregation/persistence/NewsItemPersistence.ts
@@ -3,17 +3,54 @@ import {
     IPersistenceRead,
 } from "@rocket.chat/apps-engine/definition/accessors";
 import { NewsItem } from "../definitions/NewsItem";
+import {
+    RocketChatAssociationModel,
+    RocketChatAssociationRecord,
+} from "@rocket.chat/apps-engine/definition/metadata";
+import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
+import { NewsAggregationApp } from "../NewsAggregationApp";
 
 export class NewsItemPersistence {
+    app: NewsAggregationApp;
     persistenceRead: IPersistenceRead;
     persistence: IPersistence;
 
-    constructor(persistence: IPersistence, persistenceRead: IPersistenceRead) {
+    constructor(
+        app: NewsAggregationApp,
+        persistence: IPersistence,
+        persistenceRead: IPersistenceRead,
+    ) {
+        this.app = app;
         this.persistence = persistence;
         this.persistenceRead = persistenceRead;
     }
 
-    async saveNews(news: NewsItem, id: string) {}
+    async saveNews(news: NewsItem, id: string) {
+        const associations: Array<RocketChatAssociationRecord> = [
+            new RocketChatAssociationRecord(
+                RocketChatAssociationModel.MISC,
+                "news-aggregation-subscribed-room",
+            ),
+            new RocketChatAssociationRecord(
+                RocketChatAssociationModel.MISC,
+                id,
+            ),
+        ];
 
-    async deleteNews(news: NewsItem, id: string) {}
+        let recordId: string;
+        try {
+            recordId = await this.persistence.createWithAssociations(
+                { newsItem: news },
+                associations,
+            );
+            console.log("News saved!!", recordId);
+        } catch (err) {
+            console.error(err);
+            this.app.getLogger().info(err);
+        }
+    }
+
+    async deleteNews(news: NewsItem, id: string) {
+        // const associations: Array<RocketChatAssociationRecord> = [];
+    }
 }
