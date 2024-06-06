934e8006dab8b293ef1947fba95518ea76a3e9fe
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-06 19:49:10 +0530

    changes to save News to peristence

--- news-aggregation/definitions/NewsSource.ts
+++ news-aggregation/definitions/NewsSource.ts
@@ -1,65 +1,54 @@
 import {
-    IHttp,
-    IModify,
-    IPersistence,
-    IPersistenceRead,
-    IRead,
-} from "@rocket.chat/apps-engine/definition/accessors";
+	IHttp,
+	IModify,
+	IPersistence,
+	IPersistenceRead,
+	IRead,
+} from '@rocket.chat/apps-engine/definition/accessors';
 
-import { NewsAggregationApp } from "../NewsAggregationApp";
+import { NewsAggregationApp } from '../NewsAggregationApp';
 
-import { NewsItem } from "./NewsItem";
-import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
-import { INewsSourceAdapter } from "../adapters/INewsSourceAdapter";
+import { NewsItem } from './NewsItem';
+import { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
+import { INewsSourceAdapter } from '../adapters/INewsSourceAdapter';
 
 export class NewsSource {
-    private adapter: INewsSourceAdapter;
-    app: NewsAggregationApp;
-    news: NewsItem[] = [];
+	private adapter: INewsSourceAdapter;
+	app: NewsAggregationApp;
+	news: NewsItem[] = [];
 
-    constructor(
-        app: NewsAggregationApp,
-        adapter: INewsSourceAdapter,
-        news: NewsItem[],
-    ) {
-        this.app = app;
-        this.adapter = adapter;
-        this.news = news;
-    }
+	constructor(
+		app: NewsAggregationApp,
+		adapter: INewsSourceAdapter,
+		news: NewsItem[]
+	) {
+		this.app = app;
+		this.adapter = adapter;
+		this.news = news;
+	}
 
-    async fetchNews(
-        read: IRead,
-        modify: IModify,
-        room: IRoom,
-        http: IHttp,
-        persis: IPersistence,
-    ): Promise<NewsItem[]> {
-        return this.adapter.fetchNews(read, modify, room, http, persis);
-    }
+	async fetchNews(
+		read: IRead,
+		modify: IModify,
+		room: IRoom,
+		http: IHttp,
+		persis: IPersistence
+	): Promise<NewsItem[]> {
+		return this.adapter.fetchNews(read, modify, room, http, persis);
+	}
 
-    async saveNews(
-        read: IRead,
-        modify: IModify,
-        room: IRoom,
-        http: IHttp,
-        persistence: IPersistence,
-        persistenceRead: IPersistenceRead,
-    ): Promise<any> {
-        return this.adapter.saveNews(
-            read,
-            modify,
-            room,
-            http,
-            persistence,
-            persistenceRead,
-        );
-    }
+	async saveNews(
+		persistence: IPersistence,
+		persistenceRead: IPersistenceRead
+	): Promise<any> {
+		return this.adapter.saveNews(persistence, persistenceRead);
+	}
 
-    async getNews(
-        read: IRead,
-        modify: IModify,
-        room: IRoom,
-        http: IHttp,
-        persistenceRead: IPersistenceRead,
-    ): Promise<any> {}
+	async getNews(
+		read: IRead,
+		modify: IModify,
+		room: IRoom,
+		http: IHttp,
+		persistenceRead: IPersistenceRead
+	): Promise<any> {}
 }
--- news-aggregation/persistence/NewsItemPersistence.ts
+++ news-aggregation/persistence/NewsItemPersistence.ts
@@ -1,155 +1,136 @@
 import {
-    IPersistence,
-    IPersistenceRead,
-} from "@rocket.chat/apps-engine/definition/accessors";
-import { NewsItem } from "../definitions/NewsItem";
+	IPersistence,
+	IPersistenceRead,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { NewsItem } from '../definitions/NewsItem';
 import {
-    RocketChatAssociationModel,
-    RocketChatAssociationRecord,
-} from "@rocket.chat/apps-engine/definition/metadata";
-import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
-import { NewsAggregationApp } from "../NewsAggregationApp";
+	RocketChatAssociationModel,
+	RocketChatAssociationRecord,
+} from '@rocket.chat/apps-engine/definition/metadata';
+import { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
+import { NewsAggregationApp } from '../NewsAggregationApp';
 
 export class NewsItemPersistence {
-    app: NewsAggregationApp;
-    persistenceRead: IPersistenceRead;
-    persistence: IPersistence;
+	app: NewsAggregationApp;
+	persistenceRead: IPersistenceRead;
+	persistence: IPersistence;
 
-    constructor(
-        app: NewsAggregationApp,
-        persistence: IPersistence,
-        persistenceRead: IPersistenceRead,
-    ) {
-        this.app = app;
-        this.persistence = persistence;
-        this.persistenceRead = persistenceRead;
-    }
+	constructor(
+		app: NewsAggregationApp,
+		persistence: IPersistence,
+		persistenceRead: IPersistenceRead
+	) {
+		this.app = app;
+		this.persistence = persistence;
+		this.persistenceRead = persistenceRead;
+	}
 
-    async saveNews(news: NewsItem) {
-        const associations: Array<RocketChatAssociationRecord> = [
-            new RocketChatAssociationRecord(
-                RocketChatAssociationModel.MISC,
-                "news-aggregation",
-            ),
-            new RocketChatAssociationRecord(
-                RocketChatAssociationModel.MISC,
-                news.id,
-            ),
-        ];
+	async saveNews(news: NewsItem) {
+		const associations: Array<RocketChatAssociationRecord> = [
+			new RocketChatAssociationRecord(
+				RocketChatAssociationModel.MISC,
+				'news-aggregation'
+			),
+			new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, news.id),
+		];
 
-        let recordId: string;
-        try {
-            recordId = await this.persistence.createWithAssociations(
-                { newsItem: news },
-                associations,
-            );
-            console.log("News saved!!", recordId);
-        } catch (err) {
-            console.error("Could not save news in persistence.", err);
-            this.app
-                .getLogger()
-                .error("Could not save news in persistence.", err);
-        }
-    }
+		let recordId: string;
+		try {
+			recordId = await this.persistence.createWithAssociations(
+				{ newsItem: news },
+				associations
+			);
+			console.log('News saved in Peristence!!', recordId);
+		} catch (err) {
+			console.error('Could not save news in persistence.', err);
+			this.app.getLogger().error('Could not save news in persistence.', err);
+		}
+	}
 
-    // TO-DO
-    // async saveAllNews(allNews: NewsItem[]) {
-    //     const associations: Array<RocketChatAssociationRecord> = [
-    //         new RocketChatAssociationRecord(
-    //             RocketChatAssociationModel.MISC,
-    //             "news-aggregation",
-    //         ),
-    //     ];
+	// TO-DO
+	// async saveAllNews(allNews: NewsItem[]) {
+	//     const associations: Array<RocketChatAssociationRecord> = [
+	//         new RocketChatAssociationRecord(
+	//             RocketChatAssociationModel.MISC,
+	//             "news-aggregation",
+	//         ),
+	//     ];
 
-    //     const idAssociation = allNews.map((news) => (
-    //         new RocketChatAssociationRecord(
-    //             RocketChatAssociationModel.MISC,
-    //             news.id,
-    //         )
-    //     ));
+	//     const idAssociation = allNews.map((news) => (
+	//         new RocketChatAssociationRecord(
+	//             RocketChatAssociationModel.MISC,
+	//             news.id,
+	//         )
+	//     ));
 
-    //     associations.push(idAssociation);
-    // }
+	//     associations.push(idAssociation);
+	// }
 
-    async getNewsById(news: NewsItem): Promise<object[]> {
-        const associations: Array<RocketChatAssociationRecord> = [
-            new RocketChatAssociationRecord(
-                RocketChatAssociationModel.MISC,
-                "news-aggregation",
-            ),
-            new RocketChatAssociationRecord(
-                RocketChatAssociationModel.MISC,
-                news.id,
-            ),
-        ];
+	async getNewsById(news: NewsItem): Promise<object[]> {
+		const associations: Array<RocketChatAssociationRecord> = [
+			new RocketChatAssociationRecord(
+				RocketChatAssociationModel.MISC,
+				'news-aggregation'
+			),
+			new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, news.id),
+		];
 
-        let newsObjectArray: object[];
-        try {
-            newsObjectArray =
-                await this.persistenceRead.readByAssociations(associations);
+		let newsObjectArray: object[];
+		try {
+			newsObjectArray =
+				await this.persistenceRead.readByAssociations(associations);
 
-            if (newsObjectArray.length === 0) {
-                console.error("News with the given id doesn't exist");
-                this.app
-                    .getLogger()
-                    .error("News with the given id doesn't exist");
-            }
-        } catch (err) {
-            newsObjectArray = [];
-            console.error("Could not get the desired news by id", err);
-            this.app
-                .getLogger()
-                .error("Could not get the desired news by id", err);
-        }
+			if (newsObjectArray.length === 0) {
+				console.error("News with the given id doesn't exist");
+				this.app.getLogger().error("News with the given id doesn't exist");
+			}
+		} catch (err) {
+			newsObjectArray = [];
+			console.error('Could not get the desired news by id', err);
+			this.app.getLogger().error('Could not get the desired news by id', err);
+		}
 
-        return newsObjectArray;
-    }
+		return newsObjectArray;
+	}
 
-    async removeNewsById(news: NewsItem) {
-        const associations: Array<RocketChatAssociationRecord> = [
-            new RocketChatAssociationRecord(
-                RocketChatAssociationModel.MISC,
-                "news-aggregation",
-            ),
-            new RocketChatAssociationRecord(
-                RocketChatAssociationModel.MISC,
-                news.id,
-            ),
-        ];
+	async removeNewsById(news: NewsItem) {
+		const associations: Array<RocketChatAssociationRecord> = [
+			new RocketChatAssociationRecord(
+				RocketChatAssociationModel.MISC,
+				'news-aggregation'
+			),
+			new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, news.id),
+		];
 
-        const newsToBeDeleted = await this.getNewsById(news);
+		const newsToBeDeleted = await this.getNewsById(news);
 
-        let removedNews: object[];
-        try {
-            removedNews =
-                await this.persistence.removeByAssociations(associations);
-        } catch (err) {
-            removedNews = [];
-            console.error(
-                "Could not remove desired news from persistence.",
-                err,
-            );
-            this.app
-                .getLogger()
-                .error("Could not remove desired news from persistence.", err);
-        }
-    }
+		let removedNews: object[];
+		try {
+			removedNews = await this.persistence.removeByAssociations(associations);
+		} catch (err) {
+			removedNews = [];
+			console.error('Could not remove desired news from persistence.', err);
+			this.app
+				.getLogger()
+				.error('Could not remove desired news from persistence.', err);
+		}
+	}
 
-    async removeAllNews() {
-        const associations: Array<RocketChatAssociationRecord> = [
-            new RocketChatAssociationRecord(
-                RocketChatAssociationModel.MISC,
-                "news-aggregation",
-            ),
-        ];
+	async removeAllNews() {
+		const associations: Array<RocketChatAssociationRecord> = [
+			new RocketChatAssociationRecord(
+				RocketChatAssociationModel.MISC,
+				'news-aggregation'
+			),
+		];
 
-        try {
-            await this.persistence.removeByAssociations(associations);
-        } catch (err) {
-            console.error("Could not remove all news from persistence.", err);
-            this.app
-                .getLogger()
-                .error("Could not remove all news from persistence.", err);
-        }
-    }
+		try {
+			await this.persistence.removeByAssociations(associations);
+		} catch (err) {
+			console.error('Could not remove all news from persistence.', err);
+			this.app
+				.getLogger()
+				.error('Could not remove all news from persistence.', err);
+		}
+	}
 }
