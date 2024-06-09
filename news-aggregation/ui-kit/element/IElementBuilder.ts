2b82819726aba59020df5343196a9bc5ead838c6
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-09 18:12:11 +0530

    define StaticSelectElement methods

--- news-aggregation/ui-kit/element/IElementBuilder.ts
+++ news-aggregation/ui-kit/element/IElementBuilder.ts
@@ -1,10 +1,28 @@
-import { ButtonElement } from '@rocket.chat/ui-kit';
+import {
+	ButtonElement,
+	Option,
+	StaticSelectElement,
+} from '@rocket.chat/ui-kit';
 import { ButtonParam } from './IButtonElement';
 import { ElementInteractionParam } from './IInteractionElement';
+import {
+	StaticSelectOptionParam,
+	StaticSelectParam,
+} from './IStaticSelectElement';
 
 export interface IElementBuilder {
 	createButton(
 		param: ButtonParam,
 		interactionParam: ElementInteractionParam
 	): ButtonElement;
+
+	createDropdown(
+		param: StaticSelectParam,
+		interactionParam: ElementInteractionParam
+	): StaticSelectElement;
+
+	createDropdownOptions(
+		param: StaticSelectOptionParam,
+		interactionParam: ElementInteractionParam
+	): Array<Option>;
 }
