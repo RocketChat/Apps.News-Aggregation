3c0827283e4fc914269e38c43a0befe3ad53f71b
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-09 19:47:19 +0530

    ActionBlock

--- news-aggregation/ui-kit/block/IBlockBuilder.ts
+++ news-aggregation/ui-kit/block/IBlockBuilder.ts
@@ -1,6 +1,19 @@
-import { SectionBlock } from '@rocket.chat/ui-kit';
+import { ActionsBlock, SectionBlock } from '@rocket.chat/ui-kit';
 import { SectionBlockParam } from './ISectionBlock';
+import { ActionBlockParam } from './IActionBlock';
 
 export interface IBlockBuilder {
 	createSectionBlock(param: SectionBlockParam): SectionBlock;
+
+	createActionBlock(param: ActionBlockParam): ActionsBlock;
+
+	// createPreviewBlock(
+	// 	param: PreviewBlockParam
+	// ): PreviewBlockBase | PreviewBlockWithThumb;
+
+	// createContextBlock(param: ContextBlockParam): ContextBlock;
+
+	// createInputBlock(param: InputBlockParam): InputBlock;
+
+	// createDividerBlock(blockId?: string): DividerBlock;
 }
