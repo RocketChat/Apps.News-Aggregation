1585f15d31e057c274aa4d8f153d9060d1e94b0f
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-09 19:44:27 +0530

    SectionBlock interface + method

--- news-aggregation/ui-kit/block/IBlockBuilder.ts
+++ news-aggregation/ui-kit/block/IBlockBuilder.ts
@@ -1,1 +1,6 @@
-export interface IBlockBuilder {}
+import { SectionBlock } from '@rocket.chat/ui-kit';
+import { SectionBlockParam } from './ISectionBlock';
+
+export interface IBlockBuilder {
+	createSectionBlock(param: SectionBlockParam): SectionBlock;
+}
