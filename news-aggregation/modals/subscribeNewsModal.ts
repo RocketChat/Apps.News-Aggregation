2d6cc0f8497354de7013fc86ebc7aec0507212b1
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-11 00:54:46 +0530

    basic subscribeNewsModal setup

---
+++ news-aggregation/modals/subscribeNewsModal.ts
@@ -0,0 +1,67 @@
+import { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
+import { NewsAggregationApp } from '../NewsAggregationApp';
+import { IUser } from '@rocket.chat/apps-engine/definition/users';
+import {
+	IHttp,
+	IModify,
+	IPersistence,
+	IRead,
+	IUIKitSurfaceViewParam,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { ModalEnum } from '../enums/modalEnum';
+import {
+	ButtonStyle,
+	UIKitSurfaceType,
+} from '@rocket.chat/apps-engine/definition/uikit';
+import { Block, TextObjectType } from '@rocket.chat/ui-kit';
+
+export async function subscribeNewsModal(
+	app: NewsAggregationApp,
+	room: IRoom,
+	sender: IUser,
+	read: IRead,
+	modify: IModify,
+	http: IHttp,
+	persis: IPersistence
+): Promise<IUIKitSurfaceViewParam | Error> {
+	const { elementBuilder, blockBuilder } = app.getBuilders();
+
+	const blocks: Block[] = [];
+
+	const submitButton = elementBuilder.createButton(
+		{
+			text: ModalEnum.SUBSCRIBE_NEWS_SUBMIT_BUTTON,
+			style: ButtonStyle.PRIMARY,
+			secondary: false,
+		},
+		{
+			blockId: ModalEnum.SUBSCRIBE_NEWS_MODAL_SUBMIT_BLOCK_ID,
+			actionId: ModalEnum.SUBSCRIBE_NEWS_MODAL_SUBMIT_ACTION_ID,
+		}
+	);
+
+	const closeButton = elementBuilder.createButton(
+		{
+			text: ModalEnum.SUBSCRIBE_NEWS_CLOSE_BUTTON,
+			style: ButtonStyle.DANGER,
+			secondary: true,
+		},
+		{
+			blockId: ModalEnum.SUBSCRIBE_NEWS_MODAL_CLOSE_BLOCK_ID,
+			actionId: ModalEnum.SUBSCRIBE_NEWS_MODAL_CLOSE_ACTION_ID,
+		}
+	);
+
+	return {
+		id: ModalEnum.SUBSCRIBE_VIEW_ID,
+		type: UIKitSurfaceType.MODAL,
+		title: {
+			type: TextObjectType.PLAIN_TEXT,
+			text: ModalEnum.SUBSCRIBE_NEWS_TITLE,
+			emoji: true,
+		},
+		blocks,
+		close: closeButton,
+		submit: submitButton,
+	};
+}
