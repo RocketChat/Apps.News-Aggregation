5fdb3988e33b67e473995bbaa9144ffecf3317e5
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-06-15 16:51:20 +0530

    TextDecoder error temporary fix

--- news-aggregation/definitions/IAppBuilders.ts
+++ news-aggregation/definitions/IAppBuilders.ts
@@ -1,7 +1,7 @@
-import { BlockBuilder } from '../builders/BlockBuilder';
-import { ElementBuilder } from '../builders/ElementBuilder';
+// import { BlockBuilder } from '../builders/BlockBuilder';
+// import { ElementBuilder } from '../builders/ElementBuilder';
 
-export interface IAppBuilders {
-	elementBuilder: ElementBuilder;
-	blockBuilder: BlockBuilder;
-}
+// export interface IAppBuilders {
+// 	elementBuilder: ElementBuilder;
+// 	blockBuilder: BlockBuilder;
+// }
