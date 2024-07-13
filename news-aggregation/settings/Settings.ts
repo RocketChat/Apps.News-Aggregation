4358c173856ab86557410376181564c5db6b7fd7
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-07-13 14:32:20 +0530

    provide settings and integrate the settings in extendConfig

---
+++ news-aggregation/settings/Settings.ts
@@ -0,0 +1,42 @@
+import {
+	ISetting,
+	SettingType,
+} from '@rocket.chat/apps-engine/definition/settings';
+
+export const Settings: Array<ISetting> = [
+	{
+		id: 'techcrunch',
+		type: SettingType.BOOLEAN,
+		packageValue: true,
+		required: true,
+		public: true,
+		value: true,
+		i18nLabel: 'TechCrunch',
+		i18nDescription:
+			'Setting to enable or disable the news from TechCrunch News Source',
+		i18nAlert: 'Toggle to enable or disable this news source',
+	},
+	{
+		id: 'bbc',
+		type: SettingType.BOOLEAN,
+		packageValue: true,
+		required: true,
+		public: true,
+		value: true,
+		i18nLabel: 'BBC News',
+		i18nDescription:
+			'Setting to enable or disable the news from BBC News Source',
+		i18nAlert: 'Toggle to enable or disable this news source',
+	},
+	{
+		id: 'espn',
+		type: SettingType.BOOLEAN,
+		packageValue: true,
+		required: true,
+		public: true,
+		i18nLabel: 'ESPN News',
+		i18nDescription:
+			'Setting to enable or disable the news from ESPN News Source',
+		i18nAlert: 'Toggle to enable or disable this news source',
+	},
+];
