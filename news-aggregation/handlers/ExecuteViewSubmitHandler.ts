c2c6f46e92f6eee5752cf809dc084a68ddddb808
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-07-21 21:46:24 +0530

    add RoomPersistence to store roomid

--- news-aggregation/handlers/ExecuteViewSubmitHandler.ts
+++ news-aggregation/handlers/ExecuteViewSubmitHandler.ts
@@ -13,6 +13,7 @@ import { SubscriptionPersistence } from '../persistence/SubscriptionPersistence'
 import { ModalEnum } from '../enums/modalEnum';
 import { RoomPersistence } from '../persistence/RoomPersistence';
 import { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
+import { RoomPersistence } from '../persistence/RoomPersistence';
 
 export class ExecuteViewSubmitHandler {
 	private context: UIKitViewSubmitInteractionContext;
